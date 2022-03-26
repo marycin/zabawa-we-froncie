2b0ed479503d8dc3eaca10e6ae1c15306c90ef2c
Marcin Bandos <80113567+marycin@users.noreply.github.com>
2022-03-26 19:03:59 +0100

    Slider

---
+++ prostagaleria/skrypt.js
@@ -0,0 +1,104 @@
+let tasks = document.getElementById('tasks-area');
+let showTaskButton = document.getElementById('show-button')
+let addTaskButton = document.getElementById('taskAddButton')
+
+
+let taskAdder = document.getElementById('task-adder')
+
+let taskTitle = document.getElementById('inputTitle')
+let taskDate = document.getElementById('taskDate')
+let taskType = document.getElementById("typeOfToDo")
+let taskPriority = document.getElementById("typeOfPriority")
+
+let menuHeaderButton = document.getElementById("menuheader-button")
+
+let counter = 1
+
+tasks.innerHTML += "<div class='task' id='row"+counter+"'>\
+<div class='task-left'>\
+    <input type='checkbox'>\
+    <div class='task-description'>\
+        <h3>Sample task</h3>  \
+        <p>Priority: </p> \
+        <p>Type:  </p> \
+        <p>Date: </p> \
+    </div> \
+</div>\
+<div class='task-right'>\
+    <button id='delete-task"+counter+"' class='delete-task' onclick='remove(this)' >DEL</button>\
+</div>\
+</div>"
+
+function addItem(){
+    let titleToAdd = taskTitle.value
+    let dateToAdd = taskDate.value
+    let typeToAdd = taskType.value
+    let priorityToAdd = taskPriority.value
+
+    if(titleToAdd !== "" && dateToAdd !=="" && typeToAdd !=="None" && priorityToAdd !=="None"){
+        counter++
+        tasks.innerHTML += "<div class='task' id='row"+counter+"'>\
+        <div class='task-left'>\
+            <input type='checkbox'>\
+            <div class='task-description'>\
+                <h3>"+titleToAdd+"</h3>  \
+                <p>Priority: "+priorityToAdd+"</p> \
+                <p>Type: "+typeToAdd+" </p> \
+                <p>Date: "+dateToAdd+"</p> \
+            </div> \
+        </div>\
+        <div class='task-right'>\
+            <button id='delete-task"+counter+"' class='delete-task' onclick='remove(this)' >DEL</button>\
+        </div>\
+        </div>"
+
+        taskTitle.value =""
+        taskType.value = "None"
+        taskPriority.value = "None"
+        taskAdder.style.display = "none"
+        
+    }
+    else{
+        console.log("cos nie tak")
+    }
+}
+
+function openOrShowTaskAdder(){
+    
+    if(taskAdder.style.display ==="flex"){
+        taskAdder.style.display = "none"
+    }
+    else{
+        taskAdder.style.display ="flex"
+    }
+}
+
+function remove(button){
+    let number = button.id.toString();
+    let row = document.getElementById('row'+number[number.length-1])
+    row.remove()
+}
+
+function showMenuButtons(){
+    let menubuttons = document.getElementById('menubuttons')
+    if(window.innerWidth < 501){
+        
+        if(menubuttons.style.display ==="block"){
+            menubuttons.style.display ="none"
+            taskAdder.style.top = "200px"
+        }
+        else{
+            menubuttons.style.display ="block"
+            taskAdder.style.top="30px"
+        }
+    }
+    else{
+        menubuttons.style.display="block"
+    }
+}
+
+menuHeaderButton.addEventListener('click',showMenuButtons)
+
+showTaskButton.addEventListener('click',openOrShowTaskAdder);
+
+addTaskButton.addEventListener('click',addItem);
